dc27e2f17ec6da9ef445153631e61c9cbddac5ec
chaoowang <chaohsuaan@gmail.com>
2022-04-05 23:42:25 +0800

    Implement helper method

--- app/src/main/java/au/edu/jcu/cp3406/stopwatchapp/StopwatchActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/stopwatchapp/StopwatchActivity.java
@@ -3,18 +3,65 @@ package au.edu.jcu.cp3406.stopwatchapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
 
 public class StopwatchActivity extends AppCompatActivity {
+
+    private Stopwatch time;
+    private Handler handler;
+    private boolean isRunning = false;
+    private TextView display;
+    private Button start_button;
+    private String start_text = "start";
+    private String stop_text = "stop";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        time = new Stopwatch();
+        display = findViewById(R.id.time);
+        start_button = findViewById(R.id.start);
     }
-    public void buttonClicked(){
 
+
+
+    private void enableStopwatch() {
+
+        isRunning = true;
+        handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (isRunning) {
+                    time.tick();
+                    display.setText(time.toString());
+                    handler.postDelayed(this, 1000);
+                }
+            }
+        });
     }
-    public void settingClicked(){
+
+    private void disableStopwatch() {
+        isRunning = false;
+    }
+
+    public void buttonClicked(View view) {
+
+        if (!isRunning){
+            enableStopwatch();
+            start_button.setText(stop_text);
+        }
+        else {
+            disableStopwatch();
+            start_button.setText(start_text);
+        }
+    }
+
+    public void settingClicked(View view) {
 
     }
 }
