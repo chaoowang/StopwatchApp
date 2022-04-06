3ccd50df59d8a2ebb2486aa07fbb7f87ef4dafa1
chaoowang <chaohsuaan@gmail.com>
2022-04-07 00:59:31 +0800

    Implement onSaveInstanceState method

--- app/src/main/java/au/edu/jcu/cp3406/stopwatchapp/Stopwatch.java
+++ app/src/main/java/au/edu/jcu/cp3406/stopwatchapp/Stopwatch.java
@@ -7,10 +7,14 @@ import java.util.Locale;
 public class Stopwatch {
     private int hours, minutes, seconds;
 
-    Stopwatch() {
+    public Stopwatch() {
         hours = minutes = seconds = 0;
     }
-
+    public Stopwatch(int hours, int minutes, int seconds){
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
     void tick() {
         seconds += 1;
         if (seconds == 60) {
@@ -27,5 +31,17 @@ public class Stopwatch {
     @Override
     public String toString() {
         return String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, seconds);
+    }
+
+    public int getHours(){
+        return hours;
+    }
+
+    public int getMinutes(){
+        return minutes;
+    }
+
+    public int getSeconds(){
+        return seconds;
     }
 }
--- app/src/main/java/au/edu/jcu/cp3406/stopwatchapp/StopwatchActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/stopwatchapp/StopwatchActivity.java
@@ -1,12 +1,16 @@
 package au.edu.jcu.cp3406.stopwatchapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.PersistableBundle;
 import android.widget.Button;
 import android.widget.TextView;
 import android.view.View;
+
+import java.util.ArrayList;
 
 public class StopwatchActivity extends AppCompatActivity {
 
@@ -22,12 +26,33 @@ public class StopwatchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        time = new Stopwatch();
         display = findViewById(R.id.time);
         start_button = findViewById(R.id.start);
+
+        if (savedInstanceState == null) {
+            time = new Stopwatch();
+        } else {
+            int hours = savedInstanceState.getInt("hours");
+            int minutes = savedInstanceState.getInt("minutes");
+            int seconds = savedInstanceState.getInt("seconds");
+            time = new Stopwatch(hours, minutes, seconds);
+            boolean running = savedInstanceState.getBoolean("running");
+            display.setText(time.toString());
+            if (running) {
+                enableStopwatch();
+                start_button.setText(stop_text);
+            }
+        }
     }
 
-
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("hours", time.getHours());
+        outState.putInt("minutes", time.getMinutes());
+        outState.putInt("seconds", time.getSeconds());
+        outState.putBoolean("running", isRunning);
+    }
 
     private void enableStopwatch() {
 
@@ -51,11 +76,10 @@ public class StopwatchActivity extends AppCompatActivity {
 
     public void buttonClicked(View view) {
 
-        if (!isRunning){
+        if (!isRunning) {
             enableStopwatch();
             start_button.setText(stop_text);
-        }
-        else {
+        } else {
             disableStopwatch();
             start_button.setText(start_text);
         }
